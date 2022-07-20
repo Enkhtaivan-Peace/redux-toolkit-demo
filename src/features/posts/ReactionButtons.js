45d35aa1da8e0f39c129bbcd2c6e9540e981a0ea
Enkhtaivan-Peace <mir18.enktaivan@gmail.com>
2022-07-20 13:33:52 +0800

    first commit

---
+++ src/features/posts/ReactionButtons.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useDispatch } from "react-redux";
+import { reactionAdded } from "./postsSlice";
+
+const reactionEmoji = {
+    thumbsUp:'👍',
+    wow: '😊',
+    heart: '❤',
+    rocket: '🚀',
+    coffee: '🥃'
+}
+
+
+const ReactionButtons = ({post}) => {
+    const dispatch = useDispatch()
+    const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+        return (
+            <button key={name} type="button" onClick={()=>dispatch(reactionAdded({postId: post.id, reaction: name}))}>{emoji} {post.reactions[name]}</button>
+        )
+    })
+    return (
+        <div>
+         {reactionButtons}
+        </div>
+  )
+}
+
+export default ReactionButtons
